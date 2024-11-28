71eac6c021692cfef0ae2de13eff5595f2ff339e
Luis R <78448904+Starmise@users.noreply.github.com>
2024-11-28 16:49:25 -0600

    Movement limitation based in axis added
    -Movement limitation based in axis added.
    -Modified the fabric materials
    -Added new blocks to the game in different orientations in order to test the axis limitation

--- Threads/Assets/Scripts/ThreadMovement.cs
+++ Threads/Assets/Scripts/ThreadMovement.cs
@@ -11,10 +11,13 @@ public class ThreadMovement : MonoBehaviour
     Vector3 clickOffset = Vector3.zero;
     Vector3 gridOffset = Vector3.zero;
 
+    Orientation orientation;
+
     Vector3 targetPosition;
 
     void Start()
     {
+        SetOrientation();
         SetGridOffset();
     }
 
@@ -35,8 +38,6 @@ public class ThreadMovement : MonoBehaviour
 
         if (clickPlane.Raycast(ray, out planeDistance))
         {
-           //Vector3 transformPosition = (ray.GetPoint(planeDistance)) + clickOffset;
-           //transform.position = SnapToGrid(targetPosition);
            MoveThread(ray.GetPoint(planeDistance));
         }
     }
@@ -44,9 +45,28 @@ public class ThreadMovement : MonoBehaviour
     void MoveThread(Vector3 screenPosition)
     {
         Vector3 offsetPosition = screenPosition + clickOffset;
-        Vector3 gridPosition = SnapToGrid(offsetPosition);
-        targetPosition = gridPosition;
+        Vector3 lockedPosition = LockPositionToLocalForwardAxis(offsetPosition);
+        Vector3 gridPosition = SnapToGrid(lockedPosition);
+        transform.position = gridPosition;
     }
+
+    Vector3 LockPositionToLocalForwardAxis(Vector3 pos)
+    {
+        switch (orientation)
+        {
+            case Orientation.NORTH:
+            case Orientation.SOUTH:
+                pos.x = transform.position.x;
+                break;
+            case Orientation.EAST:
+            case Orientation.WEST:
+                pos.z = transform.position.z;
+                break;
+        }
+
+        return RoundVector(pos);
+    }
+
 
     Vector3 SnapToGrid(Vector3 pos)
     {
@@ -55,7 +75,24 @@ public class ThreadMovement : MonoBehaviour
           Mathf.Floor(pos.z) + gridOffset.z
         );
 
-        return gridPosition;
+        return RoundVector(gridPosition);
+    }
+
+    void SetOrientation()
+    {
+        Vector3 localFwd = RoundVector(transform.forward);
+        Vector3 worldFwd = RoundVector(Vector3.forward);
+
+        float angle = Vector3.SignedAngle(worldFwd, localFwd, Vector3.up);
+
+        if (angle == 0)
+            orientation = Orientation.EAST;
+        else if (angle == 90)
+            orientation = Orientation.NORTH;
+        else if (angle == 180)
+            orientation = Orientation.WEST;
+        else
+            orientation = Orientation.SOUTH;
     }
 
     void SetGridOffset()
@@ -70,5 +107,22 @@ public class ThreadMovement : MonoBehaviour
     Ray GetScreenPointToRay()
     {
         return Camera.main.ScreenPointToRay(Input.mousePosition);
+    }
+
+    static Vector3 RoundVector(Vector3 v)
+    {
+        return new Vector3(
+          Mathf.Round(v.x * 2) / 2,
+          Mathf.Round(v.y * 2) / 2,
+          Mathf.Round(v.z * 2) / 2
+        );
+    }
+
+    enum Orientation
+    {
+        NORTH,
+        EAST,
+        SOUTH,
+        WEST
     }
 }
